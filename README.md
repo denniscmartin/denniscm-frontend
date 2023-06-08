e85888bca9d05f5158e3fabde25faf5279cb6d49
Dennis <dennisconcepcionmartin@gmail.com>
2023-06-08 19:14:02 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# denniscm-frontend
---
+++ src/main.js
@@ -0,0 +1,29 @@
+import { createApp } from 'vue'
+import {createRouter, createWebHistory} from 'vue-router'
+
+import '@/assets/bootstrap.scss';
+import '@/assets/custom.css';
+
+import App from '@/App.vue'
+import HomeView from "@/HomeView.vue";
+
+// Define routes
+const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: HomeView,
+    },
+]
+
+// Create router instance
+const router = createRouter({
+    history: createWebHistory(),
+    routes: routes,
+})
+
+// Create app
+const app = createApp(App)
+
+app.use(router)
+app.mount('#app')
